a533e204d20b8e1a840ebabb7020ab9cf2ad7e18
Egor_Ostroumov <araimo@yandex.ru>
2022-07-10 16:53:28 +0300

    2.3.7

--- 2.3.py
+++ 2.3.py
@@ -138,7 +138,8 @@ bob.get_info() # печатает "Worker Bob Moore; passport-1635777202"
 #     info.get_info()
 
 
-"""Ваша задача  создать класс CustomLabel, у которого есть:
+"""
+Ваша задача  создать класс CustomLabel, у которого есть:
 - метод __init__, принимающий один обязательный аргумент текст виджета, его необходимо сохранить в атрибут text. 
 И также в метод  может поступать произвольное количество именованных аргументов. Их необходимо сохранять в 
 атрибуты экземпляра под тем же названием
@@ -146,20 +147,72 @@ bob.get_info() # печатает "Worker Bob Moore; passport-1635777202"
 с указанным именем или, если этот атрибут уже присутствовал в экземпляре, изменить его на новое значение
 """
 
-# from tkinter import Label
 
-class CustomLabel:
-    def __init__(self, text, **kwargs):
-        self.text = text
-        for key, value in kwargs.items():
-            self.__dict__[key] = value
+# class CustomLabel:
+#     def __init__(self, text, **kwargs):
+#         self.text = text
+#         for key, value in kwargs.items():
+#             self.__dict__[key] = value
+#
+#     def config(self, **kwargs):
+#         self.__dict__.update(kwargs)
+#
+#
+# label = CustomLabel(text="Hello", bd=20, bg='#ffaaaa')
+# print(label.__dict__) # {'text': 'Hello', 'bd': 20, 'bg': '#ffaaaa'}
+# label.config(color='red', bd=100)
+# print(label.__dict__) # {'text': 'Hello', 'bd': 100, 'bg': '#ffaaaa', 'color': 'red'}
 
-    def config(self, **kwargs):
-        self.__dict__.update(kwargs)
+"""  
+Создайте базовый класс Person, у которого есть:
+1 метод __init__, принимающий имя и возраст человека. Их необходимо сохранить в атрибуты экземпляра name и age 
+соответственно
+2 метод display_person_info , который печатает информацию в следующем виде:
+    Person: {name}, {age}
+
+Затем создайте класс Company , у которого есть:
+1 метод __init__, принимающий название компании и город ее основания. Их необходимо сохранить в атрибуты 
+экземпляра company_name  и location соответственно
+2 метод display_company_info , который печатает информацию в следующем виде:
+    Company: {company_name}, {location}
+
+И в конце создайте класс Employee , который:
+1 имеет метод __init__, принимающий имя человека, его возраст, название компании и город основания. 
+Необходимо создать атрибут personal_data и сохранить в него экземпляр класса Person. 
+И также создать атрибут work  и сохранить в него экземпляр класса Company 
+2 После этого через атрибуты personal_data и work  вы можете обращаться к методам соответствующих
+классов Person и Company 
+"""
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_person_info(self):
+        return print(f'Person: {self.name}, {self.age}')
 
 
-label = CustomLabel(text="Hello", bd=20, bg='#ffaaaa')
-print(label.__dict__) # {'text': 'Hello', 'bd': 20, 'bg': '#ffaaaa'}
-label.config(color='red', bd=100)
-print(label.__dict__) # {'text': 'Hello', 'bd': 100, 'bg': '#ffaaaa', 'color': 'red'}
+class Company:
+    def __init__(self, company_name, location):
+        self.company_name = company_name
+        self.location = location
 
+    def display_company_info(self):
+        return print(f'Company: {self.company_name}, {self.location}')
+
+
+class Employee:
+    def __init__(self, name, age, company_name, location):
+        self.personal_data = Person(name, age)
+        self.work = Company(company_name, location)
+
+
+
+emp = Employee('Jessica', 28, 'Google', 'Atlanta')
+print(emp.personal_data.name)
+print(emp.personal_data.age)
+emp.personal_data.display_person_info()
+print(emp.work.company_name)
+print(emp.work.location)
+emp.work.display_company_info()
